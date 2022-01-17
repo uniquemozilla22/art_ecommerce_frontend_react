ec5310a8a9b63ff45354b25c6371f32478e751a2
yogeshbhattarai <yogesh@edigital.edu.np>
2022-01-18 04:28:10 +0545

    VSC

--- src/store/reducers/UserReducer.js
+++ src/store/reducers/UserReducer.js
@@ -7,7 +7,7 @@ const initialState = {
 const UserReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
-  switch (action.type) {
+  switch (type) {
     default:
       return state;
   }

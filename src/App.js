bac243d638e3abaae102d647bf5d1d703abb8b3f
yogeshbhattarai <yogesh@edigital.edu.np>
2022-01-18 09:23:39 +0545

    Updated Layout in all the routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }

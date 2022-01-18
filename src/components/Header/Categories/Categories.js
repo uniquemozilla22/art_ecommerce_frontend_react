a5635d305af05ea27e62a40d73d2ba5464e18595
yogeshbhattarai <yogesh@edigital.edu.np>
2022-01-18 09:24:10 +0545

    Categories Initiation

--- src/components/Header/Categories/Categories.js
+++ src/components/Header/Categories/Categories.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import classes from "./Categories.module.css";
+import CategoryList from "./CategoryList";
 
-const Categories = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Categories = ({ data }) => {
+  console.log(data);
+  return (
+    <div className={classes.categories__container}>
+      <div className={"container-fluid " + classes.category__lists}>
+        {data.map((category) => (
+          <CategoryList {...category} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,60 @@
-import { Grid, Container } from "@mui/material";
 import React, { useState } from "react";
 import TopTag from "./Toptag/TopTag";
 import Navbar from "./Navbar/Navbar";
+import Categories from "./Categories/Categories";
 
 const Header = () => {
   const [data, setData] = useState({
     name: "Art Gallery",
     contactNumber: "+977 9812345123",
-    logo: "https://uploads.turbologo.com/uploads/design/hq_preview_image/2973807/draw_svg20210507-22909-1fwkwcd.svg.png",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSsHzWM0xWbFVMrZ95leaNeIIcJ4_XZbIhYg&usqp=CAU",
+    categories: [
+      {
+        id: 1,
+        name: "Category 1",
+      },
+      {
+        id: 2,
+        name: "Category 2",
+      },
+      {
+        id: 3,
+        name: "Category 3",
+      },
+      {
+        id: 4,
+        name: "Category 4",
+      },
+      {
+        id: 5,
+        name: "Category 5",
+        subCategory: [
+          {
+            id: 1,
+            name: "Sub Category 1",
+          },
+          {
+            id: 2,
+            name: "Sub Category 2",
+          },
+          {
+            id: 3,
+            name: "Sub Category 3",
+          },
+          {
+            id: 4,
+            name: "Sub Category 4",
+          },
+        ],
+      },
+    ],
   });
 
   return (
     <>
       <TopTag contact={data.contactNumber} />
       <Navbar logo={data.logo} name={data.name} />
+      <Categories data={data.categories} />
     </>
   );
 };

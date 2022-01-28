d600a3c5fd2e51f55ebaa5d04b74b94b5bef0c91
yogeshbhattarai <yogesh@edigital.edu.np>
2022-01-28 15:49:25 +0545

    total calculations added

--- src/components/ProductTable/ProductTable.js
+++ src/components/ProductTable/ProductTable.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductFeild from "../ProductFeild/ProductFeild";
 import ProductItem from "./ProductItem/ProductItem";
 import classes from "./ProductTable.module.css";
 
 const ProductTable = ({ items, removeFunction }) => {
+  const [coupon, setCoupon] = useState(1400);
+  let total = 0;
   console.log(items);
   return (
     <div className={classes.product__table__container}>
@@ -15,17 +17,34 @@ const ProductTable = ({ items, removeFunction }) => {
       </div>
       <div className={classes.product__listing__container}>
         <div className={classes.product__list}>
-          {items.map((item) => (
-            <ProductItem
-              image={item.image}
-              name={item.name}
-              price={item.price}
-              supplierName={"Phurba Tamang"}
-              description={item.description}
-              edition={"18/20"}
-              removeItem={() => removeFunction(item.id)}
-            />
-          ))}
+          {items.map((item) => {
+            total = total + item.price;
+            return (
+              <ProductItem
+                image={item.image}
+                name={item.name}
+                price={item.price}
+                supplierName={"Phurba Tamang"}
+                description={item.description}
+                edition={"18/20"}
+                removeItem={() => removeFunction(item.id)}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className={classes.calculations}>
+        <div className={classes.totals}>
+          <h4>
+            Sub Total<span> {total}</span>{" "}
+          </h4>
+          <h4>
+            Coupon <span>{coupon} </span>
+          </h4>
+          <hr />
+          <h4>
+            Grand Total <span>{total - coupon}</span>
+          </h4>
         </div>
       </div>
     </div>

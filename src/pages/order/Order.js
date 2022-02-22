5fe59f1979ffa4e73606d32d9514330f74bf09d0
yogeshbhattarai <yogesh@edigital.edu.np>
2022-02-22 09:11:18 +0545

    Order Intiiation

---
+++ src/pages/order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Order.module.css";
+
+const Order = () => {
+  return (
+    <div className={classes.order}>
+      <div className="container-fluid"></div>
+    </div>
+  );
+};
+
+export default Order;

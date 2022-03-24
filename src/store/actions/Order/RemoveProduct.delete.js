a85be82cda40ca729f43ff1a5668886ea84e8f65
yogeshbhattarai <yogesh@edigital.edu.np>
2022-03-24 10:52:04 +0545

    Remove product on id in orders action.

---
+++ src/store/actions/Order/RemoveProduct.delete.js
@@ -0,0 +1,52 @@
+import axiosBase from "../../../axiosBase";
+import { hideLoading, showLoading } from "../Loading/Loading";
+import {
+  ErrorMessage,
+  SuccessMessage,
+  WarningMessage,
+} from "../Message/Message";
+
+const RemoveProductOnOrder = (order, product) => {
+  return (dispatch, getState) => {
+    dispatch(showLoading());
+    deleteRequest(order, product, getState().user.token)
+      .then((res) => {
+        dispatch(hideLoading());
+        dispatch(SuccessMessage({ message: res.data.message }));
+      })
+      .catch((error) => {
+        dispatch(hideLoading());
+        if (error.response === undefined) {
+          dispatch(
+            ErrorMessage({
+              message: "Network Error! Check Your Internet Connection",
+            })
+          );
+        } else {
+          if (error.response?.status === 401) {
+            dispatch(
+              WarningMessage({
+                message: error.response.data.message,
+              })
+            );
+          } else {
+            dispatch(
+              ErrorMessage({
+                message: error.response.data.message,
+              })
+            );
+          }
+        }
+      });
+  };
+};
+
+const deleteRequest = (order, product, token) => {
+  return axiosBase.delete("orders/item/delete/" + order + "/" + product, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+};
+
+export default RemoveProductOnOrder;

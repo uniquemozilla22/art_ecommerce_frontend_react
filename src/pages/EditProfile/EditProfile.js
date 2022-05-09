e225e96d515aa0730cd362b4a1b704a2b0ba55c0
yogeshbhattarai <yogesh@edigital.edu.np>
2022-05-09 16:50:07 +0545

    Completion of Edit Profile Page User Details and Address)

--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Tab, Spinner } from "react-bootstrap";
 import ProfileAvatar from "../../components/ProfileAvatar/ProfileAvatar";
-import userImage from "../../Assets/artist2.png";
-import art1 from "../../Assets/art1.jpg";
-import artist2 from "../../Assets/artist2.png";
-import artist1 from "../../Assets/artist1.png";
-import art3 from "../../Assets/art3.jpg";
 import classes from "./EditProfile.module.css";
 import { connect, useSelector } from "react-redux";
 import {
   ProfileNavigationLink,
   ProfileNavigationContent,
 } from "../../components/ProfileNavigationLink/ProfileNavigationLink";
-import FetchEditProfiileData from "../../store/actions/EditProfile/EditProfile.fetch";
 import { hideLoading, showLoading } from "../../store/actions/Loading/Loading";
 import UpdateProfile from "../../store/actions/EditProfile/EditProfile.update";
 import ChangePasswordAction from "../../store/actions/ChangePassword/ChangePassword.action";
@@ -22,6 +16,7 @@ import FetchUserInfo from "./../../store/actions/EditProfile/FetchUserInfo.fetch
 
 const EditProfile = (props) => {
   const [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
   const balanceState = useSelector((state) => state.user.balance);
   const [balance, setBalance] = useState(balanceState);
 
@@ -121,40 +116,39 @@ const EditProfile = (props) => {
             },
           ],
         },
-        // {
-        //   title: "My Artists",
-        //   data: [
-        //     {
-        //       id: 1,
-        //       name: "Peter Chung",
-        //       position: "Abstract Artist",
-        //       likes: 99,
-        //       image: "artist1",
-        //     },
-        //     {
-        //       id: 2,
-        //       name: "Furba Gurung",
-        //       position: "Abstract Artist",
-        //       likes: 92,
-        //       image: "artist2",
-        //     },
-        //   ],
-        // },
+        {
+          title: "My Artists",
+          data: [
+            {
+              id: 1,
+              name: "Peter Chung",
+              position: "Abstract Artist",
+              likes: 99,
+              image: "artist1",
+            },
+            {
+              id: 2,
+              name: "Furba Gurung",
+              position: "Abstract Artist",
+              likes: 92,
+              image: "artist2",
+            },
+          ],
+        },
       ],
     },
   ];
   const fetchUserData = async () => {
     const profileData = await props.FetchUserInfo();
-    const EditProfileData = await props.fetchData();
-    console.log("Edit profile data", EditProfileData);
-    setData(EditProfileData);
+    console.log("Profile profile data", profileData);
+    setUserData(profileData);
   };
 
-  return data ? (
+  return userData ? (
     <div className={"container " + classes.edit__profile__container}>
       <Tab.Container
         id="left-tabs-example"
-        defaultActiveKey={data.linkData[0].links[0].title
+        defaultActiveKey={layout[0].links[0].title
           .toLowerCase()
           .split(" ")
           .join("")}
@@ -163,12 +157,10 @@ const EditProfile = (props) => {
           <div className="col-md-3 col-sm-12">
             <ProfileAvatar
               name={
-                data.userData.googleId
-                  ? data.userData.username
-                  : "@" + data.userData.username
+                userData.googleId ? userData.username : "@" + userData.username
               }
-              image={data.userData.image_url}
-              verified={data.userData.active_status}
+              image={userData.image_url}
+              verified={userData.active_status}
               balance={balance}
             />
             <div className={classes.links__container}>
@@ -190,8 +182,8 @@ const EditProfile = (props) => {
                   links={link.links}
                   updateData={props.updateData}
                   postPassword={props.postPassword}
-                  activeStatus={data.userData.active_status}
-                  email={data.email}
+                  activeStatus={userData.active_status}
+                  email={userData.email}
                   sendOTP={props.sendOTP}
                 />
               ))}
@@ -219,7 +211,6 @@ const mapDispatchToProps = (dispatch) => {
   return {
     Loader: (data) =>
       data ? dispatch(showLoading()) : dispatch(hideLoading()),
-    fetchData: async () => await dispatch(FetchEditProfiileData()),
     FetchUserInfo: async () => await dispatch(FetchUserInfo()),
     updateData: async (data) => await dispatch(UpdateProfile(data)),
     postPassword: (data) => dispatch(ChangePasswordAction(data)),
